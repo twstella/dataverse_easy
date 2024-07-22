9de6447100213c2c4a347f4879499e389e807ac2
= <twsara@naver.com>
2024-07-22 20:29:25 +0900

    temp

--- api_call.py
+++ api_call.py
@@ -1,178 +1,210 @@
 import requests
 from datetime import datetime
 import pytz
-kst = pytz.timezone('Asia/Seoul')
+
+kst = pytz.timezone("Asia/Seoul")
 import json
 
-API_TOKEN="3533e7e1-9b59-41e7-97e5-1aed8ead7c6b"
-url="https://snu.dataverse.ac.kr/api"
-def create_dataverse(parent,name,alias,email_contacts,affiliation,description,dataverse_type):
-    dataverse_tp = dataverse_type.replace(" ","_").upper()
+API_TOKEN = "3533e7e1-9b59-41e7-97e5-1aed8ead7c6b"
+url = "https://snu.dataverse.ac.kr/api"
+
+
+def create_dataverse(
+    parent, name, alias, email_contacts, affiliation, description, dataverse_type
+):
+    dataverse_tp = dataverse_type.replace(" ", "_").upper()
     print(dataverse_tp)
-    email=[]
+    email = []
     for em in email_contacts:
-        email.append({"contactEmail":em})
+        email.append({"contactEmail": em})
     print(email)
-    data={
-  "name": name,
-  "alias": alias,
-  "dataverseContacts": email,
-  "affiliation": affiliation,
-  "description": description,
-  "dataverseType": dataverse_tp
-}
-    post = url+"/dataverses/"+parent
-    headers = {
-    "X-Dataverse-key": API_TOKEN,
-    "Content-Type": "application/json"
-}
+    data = {
+        "name": name,
+        "alias": alias,
+        "dataverseContacts": email,
+        "affiliation": affiliation,
+        "description": description,
+        "dataverseType": dataverse_tp,
+    }
+    post = url + "/dataverses/" + parent
+    headers = {"X-Dataverse-key": API_TOKEN, "Content-Type": "application/json"}
+    print(post)
+    publish = url + "/dataverses/" + alias + "/actions/:publish"
+    print(publish)
     try:
-        response = requests.post(post,headers=headers,data=json.dumps(data))
+        response = requests.post(post, headers=headers, data=json.dumps(data))
+        response = requests.post(publish, headers=headers)
         return response.json()
     except Exception as e:
         return e
 
-def create_dataset(parent,title,name,university,email_contacts,subject):
+
+def create_dataset(
+    parent, title, name, university, email_contacts, subject, description
+):
     now = datetime.now(kst)
     current_date = now.strftime("%Y-%m-%d")
     print(current_date)
-    email=[]
+    email = []
     for em in email_contacts:
-        email.append({"datasetContactEmail": {
-                                    "typeName": "datasetContactEmail",
-                                    "multiple": False,
-                                    "typeClass": "primitive",
-                                    "value": em["email"]
-                                },
-                                "datasetContactName": {
-                                    "typeName": "datasetContactName",
-                                    "multiple": False,
-                                    "typeClass": "primitive",
-                                    "value": em["owner"]
-                                }
-                            })
+        email.append(
+            {
+                "datasetContactEmail": {
+                    "typeName": "datasetContactEmail",
+                    "multiple": False,
+                    "typeClass": "primitive",
+                    "value": em["email"],
+                },
+                "datasetContactName": {
+                    "typeName": "datasetContactName",
+                    "multiple": False,
+                    "typeClass": "primitive",
+                    "value": em["owner"],
+                },
+            }
+        )
     print(email)
-    data={
-    "datasetVersion": {
-        "metadataBlocks": {
-            "citation": {
-                "fields": [
-                    {
-                        "typeName": "title",
-                        "multiple": False,
-                        "typeClass": "primitive",
-                        "value": title
-                    },
-                    {
-                        "typeName": "author",
-                        "multiple": True,
-                        "typeClass": "compound",
-                        "value": [
-                            {
-                                "authorName": {
-                                    "typeName": "authorName",
-                                    "multiple": False,
-                                    "typeClass": "primitive",
-                                    "value": name
-                                },
-                                "authorAffiliation": {
-                                    "typeName": "authorAffiliation",
-                                    "multiple": False,
-                                    "typeClass": "primitive",
-                                    "value": university
+    data = {
+        "datasetVersion": {
+            "metadataBlocks": {
+                "citation": {
+                    "fields": [
+                        {
+                            "typeName": "title",
+                            "multiple": False,
+                            "typeClass": "primitive",
+                            "value": title,
+                        },
+                        {
+                            "typeName": "author",
+                            "multiple": True,
+                            "typeClass": "compound",
+                            "value": [
+                                {
+                                    "authorName": {
+                                        "typeName": "authorName",
+                                        "multiple": False,
+                                        "typeClass": "primitive",
+                                        "value": name,
+                                    },
+                                    "authorAffiliation": {
+                                        "typeName": "authorAffiliation",
+                                        "multiple": False,
+                                        "typeClass": "primitive",
+                                        "value": university,
+                                    },
                                 }
-                            }
-                        ]
-                    },
-                    {
-                        "typeName": "datasetContact",
-                        "multiple": True,
-                        "typeClass": "compound",
-                        "value": email
-                    },
-                    {
-                        "typeName": "dsDescription",
-                        "multiple": True,
-                        "typeClass": "compound",
-                        "value": [
-                            {
-                                "dsDescriptionValue": {
-                                    "typeName": "dsDescriptionValue",
-                                    "multiple": False,
-                                    "typeClass": "primitive",
-                                    "value": "Native Dataset"
-                                },
-                                "dsDescriptionDate": {
-                                    "typeName": "dsDescriptionDate",
-                                    "multiple": False,
-                                    "typeClass": "primitive",
-                                    "value": current_date
+                            ],
+                        },
+                        {
+                            "typeName": "datasetContact",
+                            "multiple": True,
+                            "typeClass": "compound",
+                            "value": email,
+                        },
+                        {
+                            "typeName": "dsDescription",
+                            "multiple": True,
+                            "typeClass": "compound",
+                            "value": [
+                                {
+                                    "dsDescriptionValue": {
+                                        "typeName": "dsDescriptionValue",
+                                        "multiple": False,
+                                        "typeClass": "primitive",
+                                        "value": description,
+                                    },
+                                    "dsDescriptionDate": {
+                                        "typeName": "dsDescriptionDate",
+                                        "multiple": False,
+                                        "typeClass": "primitive",
+                                        "value": current_date,
+                                    },
                                 }
-                            }
-                        ]
-                    },
-                    {
-                        "typeName": "subject",
-                        "multiple": True,
-                        "typeClass": "controlledVocabulary",
-                        "value": [
-                            subject
-                        ]
-                    }
-                ]
+                            ],
+                        },
+                        {
+                            "typeName": "subject",
+                            "multiple": True,
+                            "typeClass": "controlledVocabulary",
+                            "value": [subject],
+                        },
+                    ]
+                }
             }
         }
     }
-}
-    post = url+"/dataverses/"+parent+"/datasets?doNotValidate=true"
-    headers = {
-    "X-Dataverse-key": API_TOKEN,
-    "Content-Type": "application/json"
-}
+    post = url + "/dataverses/" + parent + "/datasets?doNotValidate=true"
+    print(post)
+    headers = {"X-Dataverse-key": API_TOKEN, "Content-Type": "application/json"}
     try:
-        response = requests.post(post,headers=headers,data=json.dumps(data))
+        response = requests.post(post, headers=headers, data=json.dumps(data))
         return response.json()
     except Exception as e:
         return e
+
 
 def delete_dataverse(alias):
-    headers = {
-    'X-Dataverse-key': API_TOKEN
-}
-    delete = url+"/dataverse/"+alias
+    headers = {"X-Dataverse-key": API_TOKEN}
+    delete = url + "/dataverse/" + alias
+    print(delete)
     try:
-        response = requests.delete(delete,headers=headers)
-        return response.json()
-    except Exception as e:
-        return e
-def delete_dataset(title):
-    headers = {
-    'X-Dataverse-key': API_TOKEN
-}
-    delete = url+"/datasets/:persistentId="+title
-    try:
-        response = requests.delete(delete,headers=headers)
+        response = requests.delete(delete, headers=headers)
         return response.json()
     except Exception as e:
         return e
 
-def search_dataverse(parent):
-    headers = {
-        'X-Dataverse-key': API_TOKEN
-    }
-    get = url +"/search?q=*&subtree="+parent+"&type=dataverse&sort=date&order=desc"
+
+def delete_dataset(title):
+    headers = {"X-Dataverse-key": API_TOKEN}
+    delete = url + "/datasets/:persistentId/?persistentId=" + title
+    print(delete)
     try:
-        response = requests.get(get,headers=headers)
+        response = requests.delete(delete, headers=headers)
         return response.json()
     except Exception as e:
         return e
-def search_dataset(title):
-    headers = {
-        'X-Dataverse-key': API_TOKEN
-    }
-    get = url +"/search?q=*&subtree="+parent+"&type=dataset&sort=date&order=desc"
+
+
+def search_dataverse(parent):
+    headers = {"X-Dataverse-key": API_TOKEN}
+    get = url + "/search?q=*&subtree=" + parent + "&sort=date&order=desc"
+    print(get)
     try:
-        response = requests.get(get,headers=headers)
+        response = requests.get(get, headers=headers)
+        return response.json()
+    except Exception as e:
+        return e
+
+
+def search_dataset(parent, title):
+    headers = {"X-Dataverse-key": API_TOKEN}
+    get = (
+        (url + "/datasets/:persistentId/?persistentId=" + title)
+        if title
+        else (
+            url + "/search?q=*&subtree=" + parent + "&sort=date&order=desc&type=dataset"
+        )
+    )
+    print(get)
+    try:
+        response = requests.get(get, headers=headers)
+        return response.json()
+    except Exception as e:
+        return e
+
+
+def publish_dataset(title):
+    headers = {"X-Dataverse-key": API_TOKEN}
+    post = (
+        url
+        + "/datasets/:persistentId/actions/:publish?persistentId="
+        + title
+        + "&type=major"
+    )
+    print(post)
+    try:
+        response = requests.post(post, headers=headers)
         return response.json()
     except Exception as e:
         return e
--- dataverse_crud.py
+++ dataverse_crud.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import json
 import api_call
-import fitz
 import requests
 
 
 st.title("Dataverse 정보 입력 폼(생성 시 다 채워주세요)")
 
-data_type = st.radio("추가 유형 선택", ["Dataverse", "Dataset","File"])
-API_TOKEN="3533e7e1-9b59-41e7-97e5-1aed8ead7c6b"
-SERVER_URL="https://https://snu.dataverse.ac.kr/api"
+data_type = st.radio("추가 유형 선택", ["Dataverse", "Dataset", "File"])
+API_TOKEN = "3533e7e1-9b59-41e7-97e5-1aed8ead7c6b"
+SERVER_URL = "https://https://snu.dataverse.ac.kr/api"
 
 if data_type == "Dataverse":
     st.header("Dataverse 정보 입력")
@@ -21,38 +20,61 @@ if data_type == "Dataverse":
     email_contacts = []
     if "email_contacts" not in st.session_state:
         st.session_state.email_contacts = []
-
-    st.subheader("연락처 이메일 추가")
-    email_input = st.text_input("이메일 입력", key='email_input_dataverse')
-    if st.button("추가", key='add_email_dataverse'):
-        if email_input:
-            st.session_state.email_contacts.append(email_input)
-            st.rerun()
-        else:
-            st.warning("이메일을 입력하세요.")
-
-    if st.session_state.email_contacts:
-        st.subheader("추가된 이메일")
-        for email in st.session_state.email_contacts:
-            col1, col2 = st.columns([4, 1])
-            col1.write(email)
-            if col2.button("삭제", key=f'remove_{email}'):
-                st.session_state.email_contacts.remove(email)
+    try:
+        st.subheader("연락처 이메일 추가")
+        email_input = st.text_input("이메일 입력", key="email_input_dataverse")
+        if st.button("추가", key="add_email_dataverse"):
+            if email_input:
+                st.session_state.email_contacts.append(email_input)
                 st.rerun()
+            else:
+                st.warning("이메일을 입력하세요.")
+
+        if st.session_state.email_contacts:
+            st.subheader("추가된 이메일")
+            for email in st.session_state.email_contacts:
+                col1, col2 = st.columns([4, 1])
+                col1.write(email)
+                if col2.button("삭제", key=f"remove_{email}"):
+                    st.session_state.email_contacts.remove(email)
+                    st.rerun()
+    except Exception as e:
+        st.rerun()
     affiliation = st.text_input("소속")
 
     description = st.text_area("설명")
 
-    dataverse_type = st.selectbox("Dataverse 유형", ["Department", "Journal", "Laboratory","Organization or Institution","Researcher","Research Group","Research Project","Teaching course","Uncategorized"])
+    dataverse_type = st.selectbox(
+        "Dataverse 유형",
+        [
+            "Department",
+            "Journal",
+            "Laboratory",
+            "Organization or Institution",
+            "Researcher",
+            "Research Group",
+            "Research Project",
+            "Teaching course",
+            "Uncategorized",
+        ],
+    )
 
-    if st.button("생성", key='submit_dataverse'):
-        res = api_call.create_dataverse(parent,name,alias,st.session_state.email_contacts,affiliation,description,dataverse_type)
+    if st.button("생성", key="submit_dataverse"):
+        res = api_call.create_dataverse(
+            parent,
+            name,
+            alias,
+            st.session_state.email_contacts,
+            affiliation,
+            description,
+            dataverse_type,
+        )
         st.write("응답 내용:", res)
-    elif st.button("삭제", key='delete_dataverse'):
-        res = api_call.delete_dataverse(alias)
-        st.write("응답 내용:", res)
-    elif st.button("검색", key='search_dataverse'):
+    elif st.button("검색", key="search_dataverse"):
         res = api_call.search_dataverse(parent)
+        st.write("응답 내용:", res)
+    elif st.button("삭제", key="delete_dataverse"):
+        res = api_call.delete_dataverse(alias)
         st.write("응답 내용:", res)
 
 
@@ -65,51 +87,87 @@ elif data_type == "Dataset":
     name = st.text_input("교수 이름")
 
     university = st.text_input("소속")
+    try:
+        if "email_contacts_2" not in st.session_state:
+            st.session_state.email_contacts_2 = []
 
-    if 'email_contacts' not in st.session_state:
-        st.session_state.email_contacts = []
+        st.subheader("연락처 이메일 추가")
+        owner_name_input = st.text_input("소유자 이름", key="owner_name_input")
+        email_input = st.text_input("이메일 입력", key="email_input_dataset")
 
-    st.subheader("연락처 이메일 추가")
-    owner_name_input = st.text_input("소유자 이름", key='owner_name_input')
-    email_input = st.text_input("이메일 입력", key='email_input_dataset')
-
-    if st.button("추가", key='add_email_dataset'):
-        if email_input and owner_name_input:
-            st.session_state.email_contacts.append({"owner": owner_name_input, "email": email_input})
-            st.experimental_rerun()
-        else:
-            st.warning("이메일과 이메일 소유자 이름을 모두 입력하세요.")
-
-    if st.session_state.email_contacts:
-        st.subheader("추가된 이메일")
-        for idx, contact in enumerate(st.session_state.email_contacts):
-            col1, col2, col3 = st.columns([3, 3, 1])
-            col1.write(contact["owner"])
-            col2.write(contact["email"])
-            if col3.button("삭제", key=f'remove_{idx}'):
-                st.session_state.email_contacts.pop(idx)
+        if st.button("추가", key="add_email_dataset"):
+            if email_input and owner_name_input:
+                st.session_state.email_contacts_2.append(
+                    {"owner": owner_name_input, "email": email_input}
+                )
                 st.experimental_rerun()
+            else:
+                st.warning("이메일과 이메일 소유자 이름을 모두 입력하세요.")
 
-    subject = st.selectbox("주제 선택", ["Agricultural Sciences", "Arts and Humanities", "Astronomy and Astrophysics", "Business and Management", "Chemistry","Computer and Information Science","Earth and Environmental Sciences","Engineering","Law","Mathematical Science","Medicine, Health and Life Sciences","Physics","Social Sciences","Other"])
-    uploaded_file = st.file_uploader("PDF 파일을 업로드 하세요", type=["pdf"],accept_multiple_files=True)
-    if st.button("생성", key='submit_dataset'):
-        res = api_call.create_dataset(parent,title,name,university,st.session_state.email_contacts,subject)
+        if st.session_state.email_contacts_2:
+            st.subheader("추가된 이메일")
+            for idx, contact in enumerate(st.session_state.email_contacts_2):
+                col1, col2, col3 = st.columns([3, 3, 1])
+                col1.write(contact["owner"])
+                col2.write(contact["email"])
+                if col3.button("삭제", key=f"remove_{idx}"):
+                    st.session_state.email_contacts_2.pop(idx)
+                    st.experimental_rerun()
+    except Exception as e:
+        st.rerun()
+    description = st.text_area("설명")
+    subject = st.selectbox(
+        "주제 선택",
+        [
+            "Agricultural Sciences",
+            "Arts and Humanities",
+            "Astronomy and Astrophysics",
+            "Business and Management",
+            "Chemistry",
+            "Computer and Information Science",
+            "Earth and Environmental Sciences",
+            "Engineering",
+            "Law",
+            "Mathematical Science",
+            "Medicine, Health and Life Sciences",
+            "Physics",
+            "Social Sciences",
+            "Other",
+        ],
+    )
+    if st.button("생성", key="submit_dataset"):
+        res = api_call.create_dataset(
+            parent,
+            title,
+            name,
+            university,
+            st.session_state.email_contacts_2,
+            subject,
+            description,
+        )
         st.write("응답 내용:", res)
-    elif st.button("삭제", key='delete_dataverse'):
-        res = api_call.delete_dataverse(title)
+    elif st.button("publish", key="publish_dataset"):
+        res = api_call.publish_dataset(title)
         st.write("응답 내용:", res)
-    elif st.button("검색", key='search_dataset'):
-        res = api_call.search_dataset(title)
+    elif st.button("검색", key="search_dataset"):
+        res = api_call.search_dataset(parent, title)
         st.write("응답 내용:", res)
+    elif st.button("삭제", key="delete_dataverse"):
+        res = api_call.delete_dataset(title)
+        st.write("응답 내용:", res)
+
+
 elif data_type == "File":
     st.header("File upload")
     parent = st.text_input("상위 dataset PID")
-    uploaded_files = st.file_uploader("파일을 업로드 하세요", type=["txt", "csv", "pdf", "xlsx"], accept_multiple_files=True)
+    uploaded_files = st.file_uploader(
+        "파일을 업로드 하세요",
+        type=["txt", "csv", "pdf", "xlsx"],
+        accept_multiple_files=True,
+    )
     if st.button("파일 업로드"):
-        files = [('file', (file.name, file, file.type)) for file in uploaded_files]
-        headers = {
-        'X-Dataverse-key': API_TOKEN
-    }
+        files = [("file", (file.name, file, file.type)) for file in uploaded_files]
+        headers = {"X-Dataverse-key": API_TOKEN}
         url = f"{SERVER_URL}/api/datasets/:persistentId/add?persistentId={parent}"
         try:
             response = requests.post(url, headers=headers, files=files)
